d6407132697e85d777c81a3de39c0e1c1d933118
SHANMUGANATHAN <shanmuganathan@appikorn.com>
2025-07-24 16:45:44 +0530

    error solve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,8 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import compat from '@eslint/compat'
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
